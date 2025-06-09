1a3fc0495317b9e3c0609624ea68fe7f3883d474
Niv Aharon Buskila <94639680+NivBuskila@users.noreply.github.com>
2025-06-09 21:57:36 +0300

    Fix On Removing Matrix Rain

--- src/frontend/src/components/Login/APEXStaticLogin.tsx
+++ src/frontend/src/components/Login/APEXStaticLogin.tsx
@@ -250,24 +250,6 @@ const APEXStaticLogin: React.FC<APEXStaticLoginProps> = ({ onLoginSuccess, onReg
     <div className="relative h-screen bg-black text-white overflow-hidden">
       <MatrixRain />
       
-      {/* Matrix-like background */}
-      <div className="absolute inset-0 opacity-20">
-        {[...Array(50)].map((_, i) => (
-          <div
-            key={i}
-            className="absolute text-green-500 text-opacity-20 animate-matrix"
-            style={{
-              left: `${Math.random() * 100}%`,
-              top: `${Math.random() * 100}%`,
-              animationDelay: `${Math.random() * 5}s`,
-              fontSize: `${Math.random() * 10 + 10}px`
-            }}
-          >
-            01
-          </div>
-        ))}
-      </div>
-      
       {/* Login Content */}
       <div className="flex flex-col items-center justify-center min-h-screen p-4 relative z-10">
         {/* Logo */}
